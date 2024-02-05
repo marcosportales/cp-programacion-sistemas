ae19ed64c6d116d0357b7e3ead725d1e2170a6d5
Marcos Sebastian Portales Ramos <101372403+marcosportales@users.noreply.github.com>
2024-02-05 09:57:47 -0500

    Fix: works in linux and create a script to create static library

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <sys/stat.h>
 
 #ifdef _WIN32
-    #include <windows.h>
+#include <windows.h>
 #endif
 
 #define CONFIG_FILE "file.conf"
@@ -14,27 +14,28 @@
 #define CHECK_INTERVAL 10
 
 typedef struct {
-    char* path;
+    char *path;
 } Path;
 int lim = 0;
-Path* readConfigFile() {
-    FILE* file = fopen(CONFIG_FILE, "r");
+
+Path *readConfigFile() {
+    FILE *file = fopen(CONFIG_FILE, "r");
     if (file == NULL) {
         perror("Error opening file");
         exit(EXIT_FAILURE);
     }
 
-    Path* paths = malloc(sizeof(Path));
+    Path *paths = malloc(sizeof(Path));
     char line[256];
     int i = 0;
 
     while (fgets(line, sizeof(line), file)) {
-        if(strchr(line, '\n')){
+        if (strchr(line, '\n')) {
             line[strlen(line) - 1] = '\0';
         }
         lim++;
         paths[i].path = strdup(line);
-        
+
         i++;
         paths = realloc(paths, (i + 1) * sizeof(Path));
     }
@@ -53,35 +54,40 @@ void checkFile(Path file) {
     printf("File %s last modified time: %s\n", file.path, ctime(&temp));
     // printf("%ld\n", attrs.st_mtime > time(NULL) - CHECK_INTERVAL);
     if (attrs.st_mtime > time(NULL) - CHECK_INTERVAL) {
-        FILE* logFile = fopen(LOG_FILE, "a");
+        FILE *logFile = fopen(LOG_FILE, "a");
         if (logFile == NULL) {
             perror("Error opening log file");
             exit(EXIT_FAILURE);
         }
+
+        time_t t = time(NULL) - CHECK_INTERVAL;
+        char *fecha = ctime(&t);
+
         #ifdef _WIN32
-            char nombre[256]; 
-            DWORD tam = sizeof(nombre); 
+            char nombre[256];
+            DWORD tam = sizeof(nombre);
             BOOL resultado = GetUserName(nombre, &tam);
-            time_t t = time(NULL) - CHECK_INTERVAL;
-            char* fecha = ctime(&t);
             if (resultado)
             {
                 fprintf(logFile, "File %s was modified at time %s by user %s\n", file.path, fecha, nombre);
             }
-            else 
+            else
             {
                 fprintf(logFile, "File %s was modified at time %s but no user found\n", file.path, fecha);
             }
-
-            fclose(logFile);
+        #else
+            fprintf(logFile, "File %s was modified at time %s by user %s\n", file.path, fecha, getlogin());
         #endif
+            fclose(logFile);
+
+
     }
 }
 
 int main() {
-    Path* filesToCheck = readConfigFile();
+    Path *filesToCheck = readConfigFile();
 
-    while (1) {        
+    while (1) {
         for (int i = 0; i < lim; i++) {
             checkFile(filesToCheck[i]);
             // break;
---
+++ makeL.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/zsh
+
+gcc -c main.c -o main.o
+gcc -static 
