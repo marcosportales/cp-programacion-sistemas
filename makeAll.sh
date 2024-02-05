47a65e01e0cf3fee44f17e2c02e24c0cae4f846a
palmerodev <victor.palmero@undostres.com.mx>
2024-02-05 10:51:35 -0500

    changes on both files

---
+++ makeAll.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo ./makeL.sh
+sudo ./makeS.sh
--- makeS.sh
+++ makeS.sh
@@ -7,18 +7,18 @@ sudo chmod 777 $fileName
 
 {
 sudo echo "[Unit]"
-sudo echo "Description=pos eso"
+sudo echo "Description=Monitoring files in."
 sudo echo "After=network.start"
 
 sudo echo "[Service]"
-sudo echo "ExcStart=/usr/checkf"
+sudo echo "ExecStart=/usr/checkf"
 sudo echo "Type=simple"
 sudo echo "Restart=on-failure"
 
 sudo echo "[Install]"
-sudo echo "WantedGy=default.target"
+sudo echo "WantedBy=default.target"
 } > $fileName
 
 
-sudo systemctl daemon-reload1
+sudo systemctl daemon-reload
 sudo systemctl start $serviceName
