7c8f8297b5144a97a2d93a58857b8fd242547409
Restyled.io <commits@restyled.io>
2024-02-06 20:13:19 +0000

    Restyled by shellharden

--- checkf_make_lib.sh
+++ checkf_make_lib.sh
@@ -5,4 +5,4 @@ libPath="/usr/$libName"
 
 sudo gcc -c main.c -o main.o
 sudo ar -rv libcheckfile.a main.o
-sudo gcc -static -o $libPath main.c -I. -L. -lcheckfile
+sudo gcc -static -o "$libPath" main.c -I. -L. -lcheckfile
