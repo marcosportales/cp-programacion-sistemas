e2601f94df2c4ebea4ad2f936e0eafba01dbd179
jldestive <jldestive2@gmail.com>
2024-02-07 17:28:47 -0500

    finished

--- checkf_make_lib.ps1
+++ checkf_make_lib.ps1
@@ -2,4 +2,4 @@ $libName = "checkf"
 $libPath = ".\$libName"
 gcc -c main.c -o main.o
 ar -rv libcheckfile.a main.o
-gcc "-static" -o $libPath main.c -I. -L. -lcheckfile
+gcc -static -o $libPath main.c -I. -L. -lcheckfile
