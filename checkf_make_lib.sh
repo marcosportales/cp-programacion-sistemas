931c160998766706d046abae11bc5e7727761621
palmerodev <victor.palmero@undostres.com.mx>
2024-02-07 19:08:51 -0500

    changes on chekf_make_lib and chekf_make_service

--- checkf_make_lib.sh
+++ checkf_make_lib.sh
@@ -4,6 +4,13 @@ filesPath="/usr/checkf"
 libName="checkf"
 libPath="$filesPath/$libName"
 
+configPath="/etc/sec-info"
+
+sudo mkdir "$configPath/"
+sudo touch "$configPath/file.conf"
+sudo touch "$configPath/log.txt"
+sudo chmod 777 "$configPath/log.txt"
+
 sudo gcc -c main.c -o main.o
 sudo ar -rv libcheckfile.a main.o
 sudo gcc -static -o $libPath main.c -I. -L. -lcheckfile
