1ec92d10ed0d49e8cc97014af04919c9dd03a36e
Restyled.io <commits@restyled.io>
2024-02-08 00:10:59 +0000

    Restyled by shfmt

--- checkf_make_service.sh
+++ checkf_make_service.sh
@@ -8,31 +8,29 @@ execPath="$filesPath/$execName"
 libName="checkf"
 libPath="$filesPath/$libName"
 
-
 sudo touch "$servicePath"
 sudo chmod 777 "$servicePath"
 
 sudo touch "$execPath"
 sudo chmod 777 "$execPath"
 
-
 {
-    echo "#!/usr/bin/bash"
-    echo "sudo \"$libPath\""
+  echo "#!/usr/bin/bash"
+  echo "sudo \"$libPath\""
 } >"$execPath"
 
 {
-    sudo echo "[Unit]"
-    sudo echo "Description=Monitoring files in."
-    sudo echo "After=network.service"
+  sudo echo "[Unit]"
+  sudo echo "Description=Monitoring files in."
+  sudo echo "After=network.service"
 
-    sudo echo "[Service]"
-    sudo echo "Type=simple"
-    sudo echo "ExecStart=$execPath"
-    sudo echo "Restart=on-abnormal"
+  sudo echo "[Service]"
+  sudo echo "Type=simple"
+  sudo echo "ExecStart=$execPath"
+  sudo echo "Restart=on-abnormal"
 
-    sudo echo "[Install]"
-    sudo echo "WantedBy=default.target"
+  sudo echo "[Install]"
+  sudo echo "WantedBy=default.target"
 } >"$servicePath"
 
 sudo systemctl daemon-reload
